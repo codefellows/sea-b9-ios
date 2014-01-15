053273260a231847d7e3231285a0b920d2b6c87e
bpj823 <bradleypj@icloud.com>
2014-01-15 14:31:41 -0800

    renamed camerademo to UIImagePickerController

---
+++ UIImagePickerController/camerademo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  camerademo
+//
+//  Created by Brad on 1/15/14.
+//  Copyright (c) 2014 Brad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BPJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BPJAppDelegate class]));
+    }
+}
