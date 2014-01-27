ce86fa7517a8cc75f2bb38defc6bc66f7103017d
John Clem <johnnyclem@gmail.com>
2014-01-27 10:47:42 -0800

    added part 1 of Github to go from class demo

---
+++ GitHub To Go/GitHub To Go/CFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CFAppDelegate.h
+//  GitHub To Go
+//
+//  Created by John Clem on 1/27/14.
+//  Copyright (c) 2014 Code Fellows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHub To Go/GitHub To Go/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitHub To Go
+//
+//  Created by John Clem on 1/27/14.
+//  Copyright (c) 2014 Code Fellows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CFAppDelegate class]));
+    }
+}
